04022c111a4343debae6e22568969024b5778fcf
aliansari180 <muhammadaliansari180@gmail.com>
2023-08-12 22:33:48 +0500

    Real Estate

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import './Body.css';
+import './Body.css'; // Import the CSS file for styling
 
 const Body = () => {
   return (
-    <>
-    <div class="container">
-    <h1>OUR CARS</h1><br />
- <div class="wrapper">
-   <div class="banner-image"> </div>
-   <div class="banner-image1"> </div>
-   <div class="banner-image2"> </div>
-   <div class="banner-image3"> </div>
- 
-  </div>
-  
+    <div className="body-container">
+      <div className="text-container">
+        <h1>Eco International</h1>
+      </div>
+      <div className="card">
+        <h2>Eco International</h2>
+        <p>About our eco-friendly initiatives and projects.</p>
+      </div>
+      <div className="image-container">
+        <img src="House.jpg" alt="Large Image" />
+      </div>
+      <div className="about-card">
+        <h3>About Us</h3>
+        <p>We are dedicated to making a positive impact on the environment through our sustainable practices.</p>
+      </div>
+      <img src="icon.jpg" alt="" className='icon' />
     </div>
-   
-   
-    </>
-
   );
 };
-
-
 
 export default Body;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import './Navbar.css'; // Make sure to create this CSS file for styling
+import './Navbar.css'; // Import the CSS file for styling
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">
-      <img src="navlogo.png" alt="" />
-        <h1>
-            CAR ART 
-            </h1>
-      </div>
+      
       <ul className="nav-links">
-        <li><a href="#">TRACKS</a></li>
-        <li><a href="#">EVENTS</a></li>
+        <li><a href="#">CURRENT LOGIN</a></li>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+      </ul>
+      <div className="logo">
+        <img src="navlogo.png" alt="logo" />
+      </div>
+      <ul className="nav-links1">
+        <li><a href="#">WHAT WE BUY</a></li>
+        <li><a href="#">REFERRAL PROGRAM</a></li>
         <li><a href="#">CONTACT</a></li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
